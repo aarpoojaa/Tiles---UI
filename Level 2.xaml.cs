66b02b11a12d89350eac723a8dfe2bda51b20307
aarpoojaa <aarpoojaa@gmail.com>
2018-06-04 12:17:51 +0530

    Add files via upload

---
+++ Level 2.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Level_2.xaml
+    /// </summary>
+    public partial class Level_2 : Window
+    {
+        public Level_2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Leaderboard lead = new Leaderboard();
+            lead.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Leader leader = new Leader();
+            leader.Show();
+            this.Close();
+        }
+    }
+}
